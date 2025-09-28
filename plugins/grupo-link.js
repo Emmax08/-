ea70eec7a995701dcde11aac277d84d6cf6d1f41
Dioneibi-rip <fandubdreams@gmail.com>
2025-09-28 12:27:27 -0400

    Actualizar grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,10 +1,19 @@
-var handler = async (m, { conn, args }) => {
+var handler = async (m, { conn, isAdmin, isBotAdmin }) => {
+  if (!m.isGroup) throw '❌ Este comando solo funciona en grupos'
+  if (!isBotAdmin) throw '❌ Necesito ser *administrador* para generar el link'
+  if (!isAdmin) throw '❌ Solo los administradores pueden usar este comando'
 
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '\t\t✿:･✧ link del grupo ✧･:✿\n\n\v' + link, m, { detectLink: true })
-
+  try {
+    let group = m.chat
+    let code = await conn.groupInviteCode(group)
+    let link = 'https://chat.whatsapp.com/' + code
+    await conn.reply(m.chat, `✿:･✧ 𝐋𝐢𝐧𝐤 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨 ✧･:✿\n\n${link}`, m, { detectLink: true })
+  } catch (e) {
+    console.error(e)
+    throw '❌ No pude obtener el link del grupo, revisa que el bot sea admin'
+  }
 }
+
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = ['link', 'enlace']

2072b482012c76e40bcd2ee2e00c829db9a67dc1
Emmax08 <benitezbalta61@gmail.com>
2025-11-02 16:43:59 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -5,7 +5,6 @@ async function handler(m, { conn }) {
 
 //me la re chupan :) 
 
-    {
       const contactos = [
   {
     numero: '5217225305296',
@@ -56,7 +55,8 @@ async function handler(m, { conn }) {
     correo: 'pixelcrewteam@gmail.com',
     region: '🇨🇴 Colombia',
     web: '',
-    biografia: await conn.fetchStatus('573114910796@s.whatsapp.net').then(res => res.status).catch(_ => 'Sin biografía'),
+    biografia: await conn.fetchStatus('573114910796@s.whatsapp.net').then(res => res.status).catch(_ => 'Sin biografía)
+  }
   ]
 
   const contactArray = contactos.map(c => [

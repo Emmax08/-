86e38dc2a8ba21f503b1ac16bbc57194b1ee6600
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-02-13 11:46:18 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
+Process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
 import { setupMaster, fork } from 'cluster'
 import { checkCodesEndpoint } from './lib/apiChecker.js';

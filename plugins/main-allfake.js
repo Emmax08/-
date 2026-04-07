01ba8f7aeb824c6c1f135f6f5d98fd6454c1c099
Emmax08 <benitezbalta61@gmail.com>
2026-04-07 11:44:16 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -43,7 +43,7 @@ const iconUrl = pickRandom(iconUrls)
 global.icono = await getBuffer(iconUrl)
 
 //creador y otros
-global.creador = '12892581751@s.whatsapp.net'
+global.creador = '268998834966688@lid'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/5217225305296'
 global.namechannel = '*mᥲríᥲ k᥆ȷᥙ᥆-BOT-CHANNEL*'
